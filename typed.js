b016f44e8a5591c6a704b985cb240a89779ef2ef
danjmeehan <danjmeehan@gmail.com>
2020-09-07 21:04:24 -0500

    published typed.js settings

---
+++ typed.js
@@ -0,0 +1,12 @@
+//Typed.js settings
+var typed = new Typed(".typed-words", {
+    strings: ["Modernize the online presence of", "Efficiently market", "Automate customer acquisition for", "Get data insights about"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 1500,
+    startDelay: 500,
+    loop: true,
+    showCursor: false,
+    cursorChar: "|",
+    attr: null,
+  });
